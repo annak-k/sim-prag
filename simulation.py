32fbc0bc90cb24d5b83c37a5eaa9816b9dd29567
Anna KK <irbot314@gmail.com>
2019-05-19 19:48:32 +0100

    Add plotting function to allow combined plot

--- simulation.py
+++ simulation.py
@@ -12,55 +12,46 @@ import utilities
 
 """ Parameters """
 noise = 0.05
+perspectives = [0, 1]
 num_signals = 3
 meanings = [0, 1, 2]
 signals = ['a', 'b', 'c']
+p_learner = 1
 # num_signals = 2
 # meanings= [0, 1]
 # signals = ['a', 'b']
-perspectives = [0, 1]
 # languages = [[[1, 1], [1, 1]], [[1, 1], [1, 0]], [[1, 1], [0, 1]], [[1, 0], [1, 1]], [[1, 0], [1, 0]], [[1, 0], [0, 1]], [[0, 1], [1, 1]], [[0, 1], [1, 0]], [[0, 1], [0, 1]]]
+
+""" Generate 3x3 lexicon matrices, only if every meaning has at least one signal """
 languages = []
 for i in range(pow(2, 9), 0, -1):
     string = str(format(i, 'b'))
-    if len(string) >= 7:
+    if len(string) >= 7: # eliminate strings that don't have a signal for meaning 0
         while len(string) < 9:
-            string = '0' + string
+            string = '0' + string # pad with 0s
+        # eliminate strings that don't have a signal for meanings 1 and 2
         if string[-3:] != '000' and string[3:-3] != '000':
-            languages.append([[string[0],string[1],string[2]], [string[3],string[4],string[5]], [string[6],string[7],string[8]]])
-# generate list of language-perspective pairs
+            # create matrix
+            languages.append([[string[0],string[1],string[2]],
+                [string[3],string[4],string[5]],
+                [string[6],string[7],string[8]]])
+
+# generate list of language-perspective pairs and generate priors
 lp_pairs = []
+priors_unbiased = []
+priors_egocentric = []
 for p in perspectives:
     for l in languages:
         lp_pairs.append([l, p])
-priors = []
-for p in perspectives:
-    for l in languages:
-        priors.append(log(1/len(perspectives)) + log(1/len(languages)))
+        priors_unbiased.append(log(1/len(perspectives)) + log(1/len(languages)))
+
+        p_prior = log(0.9) if p == p_learner else log(0.1)
+        priors_egocentric.append(p_prior + log(1/len(languages)))
+
 
 def sample(posterior):
     return meanings[utilities.log_roulette_wheel(posterior)]
 
-# def reception_weights(system, signal):
-#     weights = []
-#     for row in system:
-#         weights.append(row[signal])
-#     return weights
-
-# def communicate(speaker_system, hearer_system, meaning):
-#     speaker_signal = wta(speaker_system[meaning])
-#     hearer_meaning = wta(reception_weights(hearer_system, speaker_signal))
-#     if meaning == hearer_meaning: 
-#         return 1
-#     else: 
-#         return 0
-
-# def learn(system, meaning, signal):
-#     system[meaning][signal] += 1
-
-# def train(system, ms_pair_list):
-#     for pair in ms_pair_list:
-#         learn(system, pair[0], pair[1])
 """ Given speaker's perspective and the context,
     compute a probability distribution over the referents
     of how likely the speaker is to speak about each referent
@@ -71,23 +62,30 @@ def calc_mental_state(perspective, context):
         distribution.append(log(1 - abs(perspective - o)))
     return utilities.normalize_logprobs(distribution)
 
-# TODO: Make this work
+""" Update the posterior probabilities the learner has assigned to
+    each lexicon/perspective pair based on the observed signal
+    and context """
 def update_posterior(posterior, signal, context):
     new_posterior = []
     for i in range(len(posterior)): # for each hypothesis
         language = lp_pairs[i][0]
-        ref_distribution = calc_mental_state(lp_pairs[i][1], context)
+        perspective = lp_pairs[i][1]
+
+        ref_distribution = calc_mental_state(perspective, context)
+
         marginalize = []
         for meaning in meanings:
+            # get the list of signals which can be used for the given meaning
             signals_for_r = [signals[s] for s in range(len(meanings)) if language[meaning][s] == '1']
-            num_signals_for_r = sum([int(i) for i in language[meaning]])
+            num_signals_for_r = len(signals_for_r)
+            # compute the product of the probability that the speaker chooses referent r and that signal s is produced 
             if signal in signals_for_r:
                 in_language = log((1 - noise) / num_signals_for_r)
                 marginalize.append(ref_distribution[meaning] + in_language)
             else:
                 out_of_language = log(noise / (num_signals - num_signals_for_r))
                 marginalize.append(ref_distribution[meaning] + out_of_language)
-        # print(exp(logsumexp(marginalize)))
+        
         new_posterior.append(posterior[i] + logsumexp(marginalize))
     return utilities.normalize_logprobs(new_posterior)
 
@@ -101,29 +99,75 @@ def produce(system, context):
         signal = random.choice(other_signals)
     return [signal, context]
 
+def plot_graph(results_list):
+    colors = ['darkseagreen', 'mediumpurple', 'steelblue']
+    labels = ['Most informative', 'Least informative', 'Medium informative']
+
+    for i in range(len(results_list)):
+        average = []
+        for result in results_list[i]:
+            plt.plot(result, color=colors[i], alpha=0.3)
+
+        for j in range(len(results_list[i][0])):
+            total = 0
+            for result in results_list[i]:
+                total += result[j]
+            average.append(total / len(results_list[i]))
+
+        plt.plot(average, color=colors[i], label=labels[i])
+    plt.xlabel('data points seen')
+    plt.ylabel('posterior')
+    plt.legend()
+    plt.grid()
+    plt.savefig('combined_plot.png')
+
+def simulation(speaker, no_productions, priors, hypoth_index, contexts):
+    post = deepcopy(priors)
+    posterior_list = [exp(post[hypoth_index])]
+    for i in range(no_productions):
+        d = produce(speaker, contexts[i])
+        post = update_posterior(post, d[0], d[1])
+        posterior_list.append(exp(post[hypoth_index]))
+    return posterior_list
+
 speaker1 = lp_pairs[188]
 speaker2 = lp_pairs[0]
-speaker3 = lp_pairs[10]
+speaker3 = lp_pairs[182]
 # speaker = lp_pairs[5]
-# print(lp_pairs.index([['100', '010', '001'], 0]))
-post = priors
+# print(lp_pairs.index([[['1','0','0'], ['0','1','0'], ['1','1','1']], 0]))
 contexts = [[random.random(), random.random(), random.random()] for i in range(500)]
-posterior_list1 = [exp(post[5])]
-posterior_list2 = [exp(post[5])]
-posterior_list3 = [exp(post[5])]
-for i in range(300):
-    d = produce(speaker1, contexts[i])
-    post = update_posterior(post, d[0], d[1])
-    posterior_list1.append(exp(post[188]))
+# posterior_list1 = [exp(post[188])]
+runs1 = []
+runs2 = []
+runs3 = []
+for i in range(10):
+    post_list1 = simulation(speaker1, 300, priors_unbiased, 188, contexts)
+    runs1.append(post_list1)
+    post_list2 = simulation(speaker2, 300, priors_unbiased, 0, contexts)
+    runs2.append(post_list2)
+    post_list3 = simulation(speaker3, 300, priors_unbiased, 182, contexts)
+    runs3.append(post_list3)
+all_runs = [runs1, runs2, runs3]
+plot_graph(all_runs)
+# post = deepcopy(priors_unbiased)
+# posterior_list2 = [exp(post[0])]
 # for i in range(100):
 #     d = produce(speaker2, contexts[i])
 #     post = update_posterior(post, d[0], d[1])
 #     posterior_list2.append(exp(post[0]))
+# post = deepcopy(priors_unbiased)
+# posterior_list3 = [exp(post[182])]
 # for i in range(100):
 #     d = produce(speaker3, contexts[i])
 #     post = update_posterior(post, d[0], d[1])
-#     posterior_list3.append(exp(post[10]))
-plt.plot(posterior_list1)
-# plt.plot(posterior_list2)
-# plt.plot(posterior_list3)
-plt.savefig('plot.png')
+#     posterior_list3.append(exp(post[182]))
+# plt.plot(posterior_list1, label="Most informative")
+# plt.plot(posterior_list2, label="Not informative")
+# plt.plot(posterior_list3, label="Kinda informative")
+# plt.ylabel("Posterior probability")
+# plt.xlabel("Number of data points seen")
+# plt.legend()
+# plt.savefig('plot.png')
+# print(posterior_list1)
+# print(posterior_list2)
+# print(posterior_list3)
