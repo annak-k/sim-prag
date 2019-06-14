339bb94ea8328518f8923bfdab63090ef006d00a
Anna KK <irbot314@gmail.com>
2019-06-14 14:52:56 +0100

    Create jobscript files for determining output system probs

---
+++ jobscript_857.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N spkr_prag_badlex           
+#$ -cwd                  
+#$ -l h_rt=10:00:00 
+#$ -l h_vmem=1G
+#  These options are:
+#  job name: -N
+#  use the current working directory: -cwd
+#  runtime limit of 5 minutes: -l h_rt
+#  memory limit of 1 Gbyte: -l h_vmem
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load anaconda/5.0.1
+
+# Run the program
+source activate sim-prag
+python level_prag_learn.py "speaker_857_outcome" 1 --spkr 857 --nruns 30
+source deactivate
